a120ef18a656276dbdfcf0695013f11c05b9ca9b
Jan Grau <dotjpg3141@posteo.de>
2022-08-16 19:32:55 +0200

    Make web server async

--- Co2Monitor/Co2Webserver.cs
+++ Co2Monitor/Co2Webserver.cs
@@ -9,36 +9,39 @@ using System.Threading.Tasks;
 
 namespace Co2Monitor
 {
-	internal class Co2Webserver
-	{
-		public async Task Run(DataManager manager, int port)
-		{
-			var server = new Webserver(port);
+    internal class Co2Webserver
+    {
+        public async Task Run(DataManager manager, int port)
+        {
+            var server = new Webserver(port);
 
-			if (Debugger.IsAttached)
-			{
-				OpenUrl("http://localhost:" + port);
-			}
+            if (Debugger.IsAttached)
+            {
+                OpenUrl("http://localhost:" + port);
+            }
 
-			var indexPath = "data/index.html";
-			var indexContent = File.ReadAllText(indexPath);
+            var indexPath = "data/index.html";
+            var indexContent = File.ReadAllText(indexPath);
 
-			await server.Run((context) =>
-			{
-				server.Logger.Info($"{context.Request.HttpMethod} {context.Request.Url} from {context.Request.RemoteEndPoint}");
+            await server.Run(async (context) =>
+            {
+                server.Logger.Info($"{context.Request.HttpMethod} {context.Request.Url} from {context.Request.RemoteEndPoint}");
 
-				if (Debugger.IsAttached)
-				{
-					var path = Path.GetFullPath(Path.Combine("../../..", indexPath));
-					indexContent = File.ReadAllText(path);
-				}
+                var requestUrl = context.Request.RawUrl?.ToLowerInvariant().TrimEnd('/');
 
-				var time = manager.LastTime;
-				var temperature = manager.Temperature.LastValue;
-				var co2 = manager.Co2.LastValue;
+                // consume input stream
+                var buffer = new byte[4096];
+                var input = context.Request.InputStream;
+                while (await input.ReadAsync(buffer) != 0)
+                {
+                }
 
-                using var output = new StreamWriter(context.Response.OutputStream);
-                switch (context.Request.RawUrl?.ToLowerInvariant().TrimEnd('/'))
+                var time = manager.LastTime;
+                var temperature = manager.Temperature.LastValue;
+                var co2 = manager.Co2.LastValue;
+
+                var output = new StreamWriter(context.Response.OutputStream);
+                switch (requestUrl)
                 {
                     case "/update":
                         context.Response.AddHeader("Content-Type", "application/json; charset=utf-8");
@@ -51,6 +54,12 @@ namespace Co2Monitor
                         break;
 
                     default:
+                        if (Debugger.IsAttached)
+                        {
+                            var path = Path.GetFullPath(Path.Combine("../../..", indexPath));
+                            indexContent = File.ReadAllText(path);
+                        }
+
                         output.WriteLine(indexContent
                             .Replace("$time$", ToJson(time))
                             .Replace("$temperature$", ToJson(temperature))
@@ -59,53 +68,54 @@ namespace Co2Monitor
                             .Replace("$co2History$", ToJson(manager.Co2)));
                         break;
                 }
+                await output.FlushAsync();
             });
-		}
+        }
 
-		private static string ToJson(object value)
-		{
-			return JsonConvert.SerializeObject(value, new JsonSerializerSettings()
-			{
-				ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			});
-		}
-		private static string ToJson(DataSeries series)
-		{
-			var minDate = DateTime.Today.AddDays(-7);
-			return ToJson(series.GetData().Where(item => item.Item1 >= minDate).Select(item => new
-			{
-				X = item.Item1,
-				Y = item.Item2,
-			}));
-		}
+        private static string ToJson(object value)
+        {
+            return JsonConvert.SerializeObject(value, new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            });
+        }
+        private static string ToJson(DataSeries series)
+        {
+            var minDate = DateTime.Today.AddDays(-7);
+            return ToJson(series.GetData().Where(item => item.Item1 >= minDate).Select(item => new
+            {
+                X = item.Item1,
+                Y = item.Item2,
+            }));
+        }
 
-		private static void OpenUrl(string url)
-		{
-			try
-			{
-				Process.Start(url);
-			}
-			catch
-			{
-				// hack because of this: https://github.com/dotnet/corefx/issues/10361
-				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-				{
-					url = url.Replace("&", "^&");
-					Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
-				}
-				else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-				{
-					Process.Start("xdg-open", url);
-				}
-				else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-				{
-					Process.Start("open", url);
-				}
-				else
-				{
-					throw;
-				}
-			}
-		}
-	}
+        private static void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
 }
--- Co2Monitor/Webserver.cs
+++ Co2Monitor/Webserver.cs
@@ -4,57 +4,60 @@ using System.Threading.Tasks;
 
 namespace Co2Monitor
 {
-	public class Webserver
-	{
-		private readonly HttpListener listener = new();
+    public class Webserver
+    {
+        private readonly HttpListener listener = new();
 
-		public Logger Logger { get; set; } = new Logger("Webserver");
-		public int Port { get; }
+        public Logger Logger { get; set; } = new Logger("Webserver");
+        public int Port { get; }
 
-		public Webserver(int port)
-		{
-			if (!HttpListener.IsSupported)
-			{
-				throw new PlatformNotSupportedException("HttpListener is not supportet.");
-			}
-			this.Port = port;
-			this.listener.Prefixes.Add($"http://+:{port}/");
-		}
+        public Webserver(int port)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                throw new PlatformNotSupportedException("HttpListener is not supportet.");
+            }
+            this.Port = port;
+            this.listener.Prefixes.Add($"http://+:{port}/");
+        }
 
 
-		public async Task Run(Action<HttpListenerContext> handleConnection)
-		{
-			try
-			{
-				this.listener.Start();
-			}
-			catch (HttpListenerException ex)
-			{
-				throw new Exception(@"
+        public async Task Run(Func<HttpListenerContext, Task> handleConnection)
+        {
+            try
+            {
+                this.listener.Start();
+            }
+            catch (HttpListenerException ex)
+            {
+                throw new Exception(@"
 Troubleshooting:
 https://serverfault.com/a/571817
 https://stackoverflow.com/a/18856394
 https://stackoverflow.com/a/17824452
 ".Trim(), ex);
-			}
+            }
 
-			this.Logger.Verbose($"Webserver running at port {this.Port} ...");
+            this.Logger.Verbose($"Webserver running at port {this.Port} ...");
 
-			while (this.listener.IsListening)
-			{
-				var context = await this.listener.GetContextAsync();
-				var task = Task.Run(() => handleConnection(context));
-				if (task.Exception != null)
-				{
-					this.Logger.Error(task.Exception);
-				}
-			}
-		}
+            while (this.listener.IsListening)
+            {
+                var context = await this.listener.GetContextAsync();
+                try
+                {
+                    await handleConnection(context);
+                }
+                catch (Exception ex)
+                {
+                    this.Logger.Error(ex);
+                }
+            }
+        }
 
-		public void Stop()
-		{
-			this.listener.Stop();
-			this.listener.Close();
-		}
-	}
+        public void Stop()
+        {
+            this.listener.Stop();
+            this.listener.Close();
+        }
+    }
 }
